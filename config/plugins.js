7ce01412117a65ce47d692982d5b9f816080f13a
shakil <shakil.dgency@gmail.com>
2024-03-05 17:50:51 +0600

    configure cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,22 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
   ckeditor: {
     enabled: true,
     // resolve: "./src/plugins/strapi-plugin-ckeditor",
+  },
+
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
   },
 });
